1a87dbb0b13bcfd755fed5c896471ded36067c08
Jenny Chung <jennychung16@gmail.com>
2024-11-27 16:22:32 -0800

    added calendar creation date and success toast upon creation

--- digital-advent-calendar/src/app/components/AddCalendar.js
+++ digital-advent-calendar/src/app/components/AddCalendar.js
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from 'react-toastify';
 
 export default function AddCalendar({ modalOpen, onClose, addCalendar }) {
 
@@ -17,7 +19,8 @@ export default function AddCalendar({ modalOpen, onClose, addCalendar }) {
 
         const newCalendar = {
             id: Date.now(),
-            name: calendarName
+            name: calendarName,
+            date: new Date().toLocaleString()
         };
 
         // const existingCalendars = JSON.parse(localStorage.getItem('calendars')) || [];
@@ -25,24 +28,39 @@ export default function AddCalendar({ modalOpen, onClose, addCalendar }) {
         // localStorage.setItem('calendars', JSON.stringify(existingCalendars));
         addCalendar(newCalendar);
 
-        router.push(`/calendar/${newCalendar.id}`);
+        // Show success toast upon calendar creation
+        toast.success("Calendar created successfully!", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            draggable: true,
+            progress: undefined,
+        });
 
+        setCalendarName("");
         onClose();
+
+        // Redirect to calendar page
+        setTimeout(() => {
+            router.push(`/calendar/${newCalendar.id}`);
+        }, 3500); 
     };
 
     return (
         modalOpen && 
         
         (<div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-70 z-50">
-        <div className="bg-white px-10 py-6 rounded-lg shadow-lg w-80">
-            <h1 className="text-xl font-bold text-black mb-4">Create A New Calendar</h1>
+        <div className="bg-white px-10 p-6 rounded-lg shadow-lg w-100">
+            <h1 className="text-center text-xl font-bold text-black mb-4">Create A New Calendar</h1>
             <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label htmlFor="calendarName" className="block text-sm font-medium text-black">
-                        Calendar Name
+
+                <div className="flex items-center mb-6 space-x-4">
+                    <label htmlFor="calendarName" className="block text-md font-semi-bold text-black">
+                        Calendar Name:
                     </label>
                     <input
-                        className="w-full p-2 mt-1 border border-gray-300 rounded-md text-black"
+                        className="flex-grow p-2 border border-gray-300 rounded-md text-black"
                         type="text"
                         id="calendarName"
                         value={calendarName}
@@ -51,17 +69,17 @@ export default function AddCalendar({ modalOpen, onClose, addCalendar }) {
                         onChange={(e) => setCalendarName(e.target.value)}
                     />
                 </div>
-                <div className="flex justify-center gap-3">
+                <div className="flex justify-between gap-4">
                     <button
                         type="button"
                         onClick={onClose}
-                        className="p-2 bg-gray-500 text-white rounded-md"
+                        className="p-2 bg-gray-400 hover:bg-gray-500 text-white rounded-md flex-1"
                     >
                         Cancel
                     </button>
                     <button
                         type="submit"
-                        className="p-2 bg-green-600 text-white rounded-md"
+                        className="p-2 bg-caribbean hover:bg-midnight text-white rounded-md flex-1"
                     >
                         Create Calendar
                     </button>
--- digital-advent-calendar/src/app/page.js
+++ digital-advent-calendar/src/app/page.js
@@ -1,10 +1,13 @@
 import MainPage from "./pages/MainPage";
 import Spline from '@splinetool/react-spline/next';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
     <div>
       <MainPage />
+      <ToastContainer />
     </div>
   );
 }
